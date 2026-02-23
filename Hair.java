5d0b3af11532f491c37215c2eaeb103c20d24e17
Justine <justinemationg12@gmail.com>
2026-02-23 21:44:50 +0800

    hair object

--- Hair.java
+++ Hair.java
@@ -13,6 +13,15 @@ public class Hair implements DrawingObject {
         this.pieces = new ArrayList<DrawingObject>();
 
         this.pieces.add(new SemiCircle(x, y, 126.0, 53.0, 0.0, color, false, 0.0));
+        this.pieces.add(new QuarterCircle(x-7.4, y+28, 78.2, 90.0, color, false, -54.4));
+        this.pieces.add(new QuarterCircle(x+50.9, y+28, 78.2, 90.0, color, false, 145.4));
+        this.pieces.add(new Pentagon(x+12.6, y+34.9, 40.5, 44.1, 19.845, color, 180.0));
+        this.pieces.add(new Pentagon(x+36.1, y+30.4, 40.5, 44.1, 19.845, color, 165.0));
+        this.pieces.add(new Pentagon(x+70.2, y+40.9, 27.7, 44.1, 19.845, color, 217.3));
+        this.pieces.add(new Pentagon(x+12, y+4, 40.5, 44.1, 19.845, color, -30.0));
+        this.pieces.add(new Pentagon(x+35, y-8, 40.5, 44.1, 19.845, color, -25.0));
+        this.pieces.add(new Pentagon(x+70, y-3, 40.5, 44.1, 19.845, color, 20.0));
+        this.pieces.add(new Pentagon(x+100.3, y+39.5, 40.5, 44.1, 19.845, color, 80.3));
         
     }
 
