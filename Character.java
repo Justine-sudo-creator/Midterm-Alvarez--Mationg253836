f07b4f4e0be31a011f6016ced5949c99ff5d598f
Justine <justinemationg12@gmail.com>
2026-02-24 11:24:47 +0800

    Character object

---
+++ Character.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+
+public class Character implements DrawingObject {
+    private double x, y;
+    private Head head;
+    private Torso torso;
+    private Arm leftArm;
+    private Arm rightArm;
+    private double armAngle = 0;
+
+    public Character(double x, double y, Color skin, Color hoodie, Color headphoneColor) {
+        this.x = x;
+        this.y = y;
+
+        head = new Head(x, y, skin, headphoneColor, Color.BLACK);
+        torso = new Torso(x - 34.8, y + 112.6, skin, hoodie);
+        leftArm = new Arm(x - 24.9, y + 153.5, skin, hoodie, 34.6);
+        rightArm = new Arm(x + 87.7, y + 178.5, skin, hoodie, -33);
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+        rightArm.draw(g2d);
+        leftArm.draw(g2d);
+        torso.draw(g2d);
+        head.draw(g2d);
+    }
+
+    @Override
+    public void adjust(double distance) {
+        this.x += distance;
+        
+        armAngle += 0.05;
+        double swing = Math.sin(armAngle) * 15;
+        
+        leftArm.setRotation(34.6 + swing);
+        rightArm.setRotation(-33 - swing);
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+}
